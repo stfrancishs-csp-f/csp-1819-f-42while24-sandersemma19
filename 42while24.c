22a3f6496b8f4cd4a4d8dd4972b6b561473fee5e
Emma Sanders <19sanders@stfrancishs.org>
2018-09-18 17:09:53 +0000

    sum and loop

--- 42while24.c
+++ 42while24.c
@@ -2,21 +2,28 @@
 int main(void)
 {
     int sum=0;
-    int k;
+    int k=0;
 
-    k=0;
+    //for (k=0; k<5; k++)
     while (k<5)
-
-
     {
-        sum=sum+k; //use something different in "while" version
+        //use something different in "while" version
+        printf ("%d\n",k);
+        sum=sum+k;
         k ++;
     }
 
-    printf ("%d\n",sum);
 
-    {
+    //%d makes the value an integer
+    // the \n makes a new line
+    printf ("sum - %d\n",sum);
+
+    //k=5;
+
+    //while (k>0)
     for (k=9; k>=0;k--)
+    {
+        //k=k-1 //use something different is "for"
         printf ("%d\n", k);
     }
 
